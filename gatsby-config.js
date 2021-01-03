a7887a6911877194bade21619c7ec67d37d2be16
flaviamedici <m.olivares8915@edmail.edcc.edu>
2021-01-02 21:11:17 -0800

    Added gastby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,62 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
 
 module.exports = {
+  siteMetadata: {
+    title: "My LifeStyle Blog",
+    description: "My personal blog",
+    url: "https://flaviamedici.com",
+    image: "/sunset.png",
+    twitterUserName: "@flavia_o_m",
+    author: "Flavia Medici"
+  },
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `roboto mono`,
+          `muli\:400,400i,700,700i`,
+        ],
+        display: "swap",
+      },
+    },
+  ],
 }
+
